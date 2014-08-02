ec99d604c82e824b57444d51b5130fc9a3369dba
psdmac-Mark <mark@deansoft.com.tw>
2014-08-03 01:03:01 +0800

    add lesson

--- coffeescripts/angular/path.js.coffee
+++ coffeescripts/angular/path.js.coffee
@@ -45,4 +45,34 @@ myApp.controller 'PathCtrl', ['$scope','$rootScope','$location','ParseData', ($s
       value: false
     }
   ]
+  $scope.results = [
+    {
+      title: '了解 Web 運作'
+      link: 'http://courses.tutsplus.com/courses/tools-of-the-modern-web-developer'
+      rank: 'Beginner'
+      image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-54.png'
+      price: 'Free'
+    }
+    {
+      title: '前端開發簡介'
+      link: 'https://www.youtube.com/watch?v=jgclkz6l88I'
+      rank: 'Beginner'
+      image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-106275.png'
+      price: 'Free'
+    }
+    {
+      title: 'HTML & CSS'
+      link: 'http://courses.tutsplus.com/courses/30-days-to-learn-html-css'
+      rank: 'Beginner'
+      image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-40.png'
+      price: 'Free'
+    }
+    {
+      title: 'jQuery for Designer'
+      link: 'http://courses.tutsplus.com/courses/jquery-for-designers'
+      rank: 'Beginner'
+      image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-101176.png'
+      price: '$25'
+    }
+  ]
 ]
--- js/angular/path.js
+++ js/angular/path.js
@@ -24,7 +24,7 @@ myApp.controller('PathCtrl', [
         zh_tw: 'Game'
       }
     ];
-    return $scope.components = [
+    $scope.components = [
       {
         zh_tw: '購物車',
         value: false
@@ -37,6 +37,33 @@ myApp.controller('PathCtrl', [
       }, {
         zh_tw: '後台管理',
         value: false
+      }
+    ];
+    return $scope.results = [
+      {
+        title: '了解 Web 運作',
+        link: 'http://courses.tutsplus.com/courses/tools-of-the-modern-web-developer',
+        rank: 'Beginner',
+        image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-54.png',
+        price: 'Free'
+      }, {
+        title: '前端開發簡介',
+        link: 'https://www.youtube.com/watch?v=jgclkz6l88I',
+        rank: 'Beginner',
+        image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-106275.png',
+        price: 'Free'
+      }, {
+        title: 'HTML & CSS',
+        link: 'http://courses.tutsplus.com/courses/30-days-to-learn-html-css',
+        rank: 'Beginner',
+        image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-40.png',
+        price: 'Free'
+      }, {
+        title: 'jQuery for Designer',
+        link: 'http://courses.tutsplus.com/courses/jquery-for-designers',
+        rank: 'Beginner',
+        image: 'https://cms-assets.tutsplus.com/legacy-courses/CRS-101176.png',
+        price: '$25'
       }
     ];
   }
